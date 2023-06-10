387b8b7eb9b45be2d58e3b5f8dff66324b2fdadd
M.Emre Tezcan <Tezcan_emre@icloud.com>
2023-06-10 20:48:45 +0300

    Initial Commit

---
+++ StudyProject/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  StudyProject
+//
+//  Created by Tezcan on 10.06.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
