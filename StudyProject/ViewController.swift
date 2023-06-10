61f61e0499a4edba2083f14168997f3b007d1c97
M.Emre Tezcan <Tezcan_emre@icloud.com>
2023-06-10 22:03:55 +0300

    main design without codes

--- StudyProject/ViewController.swift
+++ StudyProject/ViewController.swift
@@ -8,7 +8,17 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var userNameLabel: UILabel!
+    @IBOutlet weak var passwordLabel: UILabel!
+    @IBOutlet weak var passwordCheckLabel: UILabel!
+    @IBOutlet weak var defaultLabel1: UILabel!
+    @IBOutlet weak var defaultLabel2: UILabel!
+    @IBOutlet weak var savedDataLabel1: UILabel!
+    @IBOutlet weak var savedDataLabel2: UILabel!
+    @IBOutlet weak var usernameTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    @IBOutlet weak var passwordCheckTextField: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
---
+++ StudyProject/secondViewController.swift
@@ -0,0 +1,32 @@
+//
+//  secondViewController.swift
+//  StudyProject
+//
+//  Created by Tezcan on 10.06.2023.
+//
+
+import UIKit
+
+class secondViewController: UIViewController {
+    @IBOutlet weak var secondVCmainLabel: UILabel!
+    @IBOutlet weak var sysQuestionLabel: UILabel!
+    @IBOutlet weak var userStatisticLabel: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
